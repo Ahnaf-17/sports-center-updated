32e849c5f6721dd7189717e22901b0ad74c8b08a
unknown <ahnaf.ahmed.42173@gmail.com>
2023-10-07 23:51:14 +0600

    data fetched complete

--- src/Pages/Home/ServiceDetails.jsx
+++ src/Pages/Home/ServiceDetails.jsx
@@ -1,8 +1,16 @@
+import { useParams } from "react-router-dom";
+import Navbar from "../Common_Pages/Navbar/Navbar";
 
 const ServiceDetails = () => {
+    const {id} = useParams()
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <div className="col-span-3">
+                    <h2>news Details</h2>
+                    <p>{id}</p>
+                </div>
+                <div></div>
         </div>
     );
 };
